1ab96e0efc94b28ef91521db1084ae14cd0953ae
ChloeArd <chloe.ardoise@gmail.com>
2021-01-28 14:07:56 +0100

    Exercice 158

---
+++ script.php
@@ -0,0 +1,12 @@
+<?php
+
+$minimum = $_POST['minimum'];
+$maximum = $_POST['maximum'];
+
+if (!is_numeric($minimum) && !is_numeric($maximum) || $maximum < $minimum){
+    header("Location: index.php?error=1");
+}
+
+
+$random = rand($minimum, $maximum);
+echo "Nombre aléatoire généré : ".$random;
